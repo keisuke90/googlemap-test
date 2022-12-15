e4675582595cdf6009083e67540f49f9fc1da4a1
KeisukeKuwahara <kuwa.90k@gmail.com>
2022-12-15 22:21:46 +0900

    youtubeを表示する

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,20 @@
   >
     <Marker :options="{ position: center }" />
   </GoogleMap>
+  <YouTube
+    src="https://www.youtube.com/watch?v=jNQXAC9IVRw"
+    @ready="onReady"
+    ref="youtube"
+  />
 </template>
 
 <script setup>
 import { GoogleMap, Marker } from "vue3-google-map";
+import YouTube from "vue3-youtube";
 
 const center = { lat: 40.689247, lng: -74.044502 };
 const apiKey = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
+const onReady = () => {
+  this.$refs.youtube.playVideo();
+};
 </script>
