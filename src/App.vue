db7bba00843349cefd7586ecb32673ba462396af
KeisukeKuwahara <kuwa.90k@gmail.com>
2022-12-15 22:51:45 +0900

    ライブカメラのURLを管理するファイルを定義

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,18 @@
   >
     <Marker :options="{ position: center }" />
   </GoogleMap>
-  <YouTube
-    src="https://www.youtube.com/watch?v=jNQXAC9IVRw"
-    @ready="onReady"
-    ref="youtube"
-  />
+  <YouTube :src="cameras" @ready="onReady" ref="youtube" />
 </template>
 
 <script setup>
 import { GoogleMap, Marker } from "vue3-google-map";
 import YouTube from "vue3-youtube";
+import { liveCameras } from "./live-cameras";
 
 const center = { lat: 40.689247, lng: -74.044502 };
 const apiKey = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
 const onReady = () => {
   this.$refs.youtube.playVideo();
 };
+const cameras = liveCameras[0].live;
 </script>
---
+++ src/live-cameras.js
@@ -0,0 +1,6 @@
+export const liveCameras = [
+  {
+    location: "東京 渋谷",
+    live: "https://www.youtube.com/watch?v=3kPH7kTphnE",
+  },
+];
