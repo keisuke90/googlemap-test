65864d8ff893c34b12c3740ba3c2846bd09cbf60
KeisukeKuwahara <kuwa.90k@gmail.com>
2022-12-16 22:55:35 +0900

    ピンをクリックしたときにモーダルを表示する

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,25 @@
     <Marker
       v-for="camera in cameras"
       :options="{ position: { lat: camera.lat, lng: camera.lng } }"
+      @click="showModal()"
     />
   </GoogleMap>
-  <YouTube :src="cameras" @ready="onReady" ref="youtube" />
+
+  <video-modal :isVisible="modalVisible" @close="closeModal">
+    <YouTube
+      :src="'https://www.youtube.com/watch?v=jNQXAC9IVRw&t=11s'"
+      @ready="onReady"
+      ref="youtube"
+    />
+  </video-modal>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { GoogleMap, Marker } from "vue3-google-map";
 import YouTube from "vue3-youtube";
 import { liveCameras } from "./live-cameras";
+import videoModal from "./components/videoModal.vue";
 
 const center = { lat: 38.0, lng: 140.0 };
 const apiKey = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
@@ -24,4 +34,12 @@ const onReady = () => {
   this.$refs.youtube.playVideo();
 };
 const cameras = liveCameras;
+
+let modalVisible = ref(false);
+const showModal = () => {
+  modalVisible.value = true;
+};
+const closeModal = () => {
+  modalVisible.value = false;
+};
 </script>
---
+++ src/components/videoModal.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+interface Props {
+  isVisible: boolean;
+}
+interface Emits {
+  (event: "close"): void;
+}
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+const close = (): void => {
+  emit("close");
+};
+</script>
+
+<template>
+  <teleport to="body">
+    <div
+      class="modal"
+      id="sample-modal"
+      v-show="isVisible"
+      @click="close"
+    ></div>
+    <div class="modal-content" v-show="isVisible"><slot /></div>
+  </teleport>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.modal-content {
+  position: fixed;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  height: auto;
+  border-radius: 20px;
+}
+</style>
