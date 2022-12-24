a7cd3af755e4b4c39898f4c6e6316320c3298565
KeisukeKuwahara <kuwa.90k@gmail.com>
2022-12-24 18:10:32 +0900

    ボタンのスタイル変更

--- src/components/locationLists.vue
+++ src/components/locationLists.vue
@@ -22,15 +22,24 @@ const tagClick = (camera) => {
   padding: 20px 10px;
   display: flex;
   flex-wrap: wrap;
+  align-items: center;
   background-color: RGB(138, 180, 248);
 }
 .location {
+  display: flex;
+  align-items: center;
+  font-weight: bold;
   list-style: none;
-  background-color: rgb(11, 4, 144);
   color: white;
-  border-radius: 10px;
   padding: 10px;
   margin: 5px;
   cursor: pointer;
+  height: 50px;
+  border-radius: 24px;
+  background: #8ab4f8;
+  box-shadow: -5px 5px 10px #7194cb, 5px -5px 10px #a3d4ff;
+}
+.location:active {
+  box-shadow: inset -5px 5px 10px #7194cb, inset 5px -5px 10px #a3d4ff;
 }
 </style>
